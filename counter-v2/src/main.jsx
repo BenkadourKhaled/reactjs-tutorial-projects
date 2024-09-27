9a94a7c9cd7d71a068856fc8699944d3bbe54002
benkadourKhaled <benkkadour.khaled@gmail.com>
2024-09-27 16:21:59 +0100

    Nothing just comment added to the projects

--- counter-v2/src/main.jsx
+++ counter-v2/src/main.jsx
@@ -3,17 +3,42 @@ import React, { Component } from "react";
 class Main extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      number: 0,
+    };
   }
+
+  increment = () => {
+    this.setState({ number: this.state.number + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ number: this.state.number - 1 });
+  };
+
+  neutral = () => {
+    this.setState({ number: 0 });
+  };
 
   render() {
     return (
-      <div style={{marginTop: '250px'}}>
-        <h1>React Counter</h1>
-        <h1>0</h1>
-        <button className="btn btn-success">Increment</button>
-        <button className="btn btn-danger">Decrement</button>
-        <button className="btn btn-primary">Neutral</button>
+      <div className="row justify-content-center">
+        <div
+          style={{ marginTop: "250px", border: "5px solid black" }}
+          className="col-md-5"
+        >
+          <h1>React Counter</h1>
+          <h1>{this.state.number}</h1>
+          <button className="btn btn-success" onClick={this.increment}>
+            Increment
+          </button>
+          <button className="btn btn-danger" onClick={this.decrement}>
+            Decrement
+          </button>
+          <button className="btn btn-primary" onClick={this.neutral}>
+            Neutral
+          </button>
+        </div>
       </div>
     );
   }
